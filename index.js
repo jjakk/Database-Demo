d28caaf233762e17d25fa6ddc3feef264810522a
Jak <jaksawesome10@gmail.com>
2020-03-16 21:13:59 -0400

    stuff

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const pug = require('pug');
 const MongoClient = require('mongodb').MongoClient;
 const app = express();
 const port = process.env.PORT || 8000;
-const mongoUrl = process.env.MONGODB_URI || "mongodb://localhost:27017/mydb";
+const mongoUrl = "mongodb://localhost:27017/mydb";
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
@@ -12,26 +12,30 @@ app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
-var data;
-MongoClient.connect(mongoUrl, {useUnifiedTopology: true}, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("mydb");
-  var myobj = { name: "Hello World", address: "Highway 37" };
-  dbo.collection("customers").insertOne(myobj, function(err, res) {
-    if (err) throw err;
-    console.log("1 document inserted");
-    db.close();
-  });
-  dbo.collection("customers").findOne({}, function(err, result) {
-    if (err) throw err;
-    console.log(result);
-    data = result;
-    db.close();
-  });
-});
-
 app.get('/', function(req, res){
-  res.render('index.pug', {data: data.name});
+  MongoClient.connect(mongoUrl, {useUnifiedTopology: true}, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("mydb");
+    var query = {};
+    dbo.collection("users").find(query).toArray(function(err, result) {
+      if (err) throw err;
+      res.render('index.pug', {users: result});
+      db.close();
+    });
+  });
 }).listen(port);
+
+app.post('/createUser', function(req, res){
+  MongoClient.connect(mongoUrl, {useUnifiedTopology: true}, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("mydb");
+    var myobj = { firstName: req.body.firstName, lastName: req.body.lastName, age: req.body.age };
+    dbo.collection("users").insertOne(myobj, function(err, res) {
+      if (err) throw err;
+      db.close();
+    });
+  });
+  res.redirect('/');
+});
 
 console.log('Running on port ' + port);
